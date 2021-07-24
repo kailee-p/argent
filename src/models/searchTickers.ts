7484ea01fbd011fe6fd9a73b640c1f2aa448aa17
Kailee Pedersen <kaileepedersen@gmail.com>
2021-07-24 03:59:37 -0400

    fetch request to polygon api working locally

--- src/models/searchTickers.ts
+++ src/models/searchTickers.ts
@@ -1,5 +1,5 @@
 import { HandlerContext, HandlerEvent } from '@netlify/functions'
-const queryString = require('query-string');
+import fetch from "node-fetch";
 
 exports.handler = async (event: HandlerEvent, context: HandlerContext) => {
   //post request only
@@ -9,6 +9,11 @@ exports.handler = async (event: HandlerEvent, context: HandlerContext) => {
 
   //retrieve search input from post body
   const input = event.body;
+
+  //fetch request to Polygon API for tickers
+  fetch(`https://api.polygon.io/v3/reference/tickers?search=${input}&active=true&sort=ticker&order=asc&limit=10&apiKey=${process.env.POLYGON_API_KEY}`)
+    .then(res => res.json())
+    .then(searchResults => console.log(searchResults))
 
   return {
     statusCode: 200,
