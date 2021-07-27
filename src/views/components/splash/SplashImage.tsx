23937cda33f9e5fb58765fcf95fb1f1d2102c51c
Kailee Pedersen <kaileepedersen@gmail.com>
2021-07-27 18:25:52 -0400

    resized splash image, added lazy loader, removed unnecessary npm packages

--- src/views/components/splash/SplashImage.tsx
+++ src/views/components/splash/SplashImage.tsx
@@ -1,12 +1,15 @@
+import LazyLoad from 'react-lazyload';
 import splashImage from '../../../images/argent-splash-image.jpg';
 import styles from '../../../css/SplashImage.module.css';
 
 const SplashImage = () => {
   return (
-    <img
+    <LazyLoad>
+      <img
       className={styles.splashImage}
       src={splashImage} 
       alt="An abstract design, shifting shapes of blue, purple, white, and black in a quartzlike/iridescent pattern" />
+    </LazyLoad>
   )
 }
 
